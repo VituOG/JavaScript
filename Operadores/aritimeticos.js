3a66b57227a10a8cafab60529819429e985b204e
VituOG <vitor.carv2007@gmail.com>
2025-01-29 08:19:06 -0300

    Initial commit

---
+++ Operadores/aritimeticos.js
@@ -0,0 +1,25 @@
+console.log("Hello, World")
+
+let nome = "juliana"
+var nomes = "juliana"
+
+console.log(nome)
+
+let idade = 24 
+console.log("Minha idade é ", idade, " anos")
+
+
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
